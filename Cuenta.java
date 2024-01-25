12a702a654123f9a8c1ee4a7c3f1b25832a30932
babyyo2002 <mariaromero80@gmail.com>
2024-01-25 20:40:04 +0100

    Primer commit

---
+++ Cuenta.java
@@ -0,0 +1,86 @@
+public class Cuenta {
+    private String  nombre;
+    private String numeroCuenta;
+    private double tipoInteres;
+    private double saldo;
+
+
+
+    public Cuenta() {
+        super();
+    }
+
+    public Cuenta(Cuenta copia) {
+        super();
+        this.nombre = copia.getNombre();
+        this.numeroCuenta = copia.getNumeroCuenta();
+        this.tipoInteres = copia.getTipoInteres();
+        this.saldo = copia.getSaldo();
+    }
+
+    public Cuenta(String nombre, String numeroCuenta, double tipoInteres, double saldo) {
+        super();
+        this.nombre = nombre;
+        this.numeroCuenta = numeroCuenta;
+        this.tipoInteres = tipoInteres;
+        this.saldo = saldo;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean ingreso(double importe) {
+        boolean resultado = false;
+        if(importe >= 0) {
+            this.saldo += importe;
+            resultado = true;
+        }
+        return resultado;
+    }
+
+    public boolean reintegro(double importe) {
+        boolean resultado = false;
+        if(importe >= 0) {
+            if(this.saldo >= importe) {
+                this.saldo -= importe;
+                resultado=true;
+            }
+        }
+        return resultado;
+    }
+
+    public boolean transferencia(Cuenta destino, double importe) {
+        boolean resultado = false;
+        if(this.saldo >= importe) {
+            reintegro(importe);
+            destino.ingreso(importe);
+            resultado = true;
+        }
+        return resultado;
+    }
+
+
+}
